1368f273bfd862b3338c2592fe55ffaf2cc51fcf
Dima Sergeev <web.sergeev.dima@gmail.com>
2023-04-12 00:17:02 +0300

    completed top slider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import scrollUp from "./modules/scrollUp";
 import accordion from './modules/accordion';
+import topSlider from "./modules/topSlider";
 
 scrollUp();
 accordion();
+topSlider();
---
+++ src/modules/topSlider.js
@@ -0,0 +1,64 @@
+const topSlider = () => {
+    const slider = document.querySelector('.top-slider');
+    const slides = [...slider.querySelectorAll('.item')];
+    const dots = document.querySelector('.top-slider__dots');
+    const dotsItems = [...dots.querySelectorAll('.top-slider__dots-item')];
+    const conf = {
+        timeout: 3000,
+        active: 0
+    };
+
+    let intervalSlider;
+
+    const init = (startActive = conf.active) => {
+        conf.active = startActive;
+        showSlide();
+        startSlider();
+    }
+
+    const showSlide = () => {
+
+        let isSetActive = false;
+
+        slides.map((item, index) => {
+            if (conf.active === index && !isSetActive) {
+
+                isSetActive = true;
+                item.classList.add('active');
+                dotsItems[index].classList.add('active');
+
+                if (index < slides.length - 1) {
+                    conf.active = conf.active + 1;
+                } else {
+                    conf.active = 0;
+                }
+
+            } else {
+                item.classList.remove('active');
+                dotsItems[index].classList.remove('active');
+            }
+
+        });
+    };
+
+    const startSlider = () => {
+        intervalSlider = setInterval(() => {
+            showSlide();
+        }, conf.timeout);
+    };
+
+    init();
+
+    dots.addEventListener('click', (e) => {
+        const dot = e.target.closest('.top-slider__dots-item');
+
+        if(dot) {
+            const index = dot.getAttribute('data-index');
+            clearInterval(intervalSlider);
+            init(+index)
+        }
+
+    });
+}
+
+export default topSlider;
