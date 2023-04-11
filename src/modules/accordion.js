597350fea5c95ffbf1333314f7a0a838646fa674
Dima Sergeev <web.sergeev.dima@gmail.com>
2023-04-11 22:58:58 +0300

    completed accordion

--- src/modules/accordion.js
+++ src/modules/accordion.js
@@ -1,9 +1,26 @@
 const accordion = () => {
+
     const accordion = document.querySelector('.accordeon');
+
+    const hiddenElements = () => {
+        const elements = [...accordion.querySelectorAll('.element')];
+        elements.map(item => {
+            item.classList.remove('active');
+            item.querySelector('.element-content').style.display = 'none';
+        })
+    }
 
     accordion.addEventListener('click', e => {
 
-    })
+        const element = e.target.closest('.element:not(.active)');
+
+        if (element) {
+            hiddenElements();
+            element.classList.add('active');
+            element.querySelector('.element-content').style.display = 'block';
+        }
+
+    });
 }
 
 export default accordion;
